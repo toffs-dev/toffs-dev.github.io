b6afddc5836424b2a80f96a5f123719db38b313d
phamthelong <39481520+phamthelong@users.noreply.github.com>
2023-07-18 08:25:13 +0000

    update menu

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,21 @@
 "use strict";
 
-
+$( document ).ready(function() {
+	$('.active a.clickable').on("click", function (e) {
+		if ($(this).hasClass('panel-collapsed')) {
+			// expand the panel
+			$(this).parents('.active').find('.collapsein').slideDown();
+			$(this).removeClass('panel-collapsed');
+			$(this).find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+		}
+		else {
+			// collapse the panel
+			$(this).parents('.active').find('.collapsein').slideUp();
+			$(this).addClass('panel-collapsed');
+			$(this).find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+		}
+	});
+});
 /* ===== Stickyfill ===== */
 /* Ref: https://github.com/wilddeer/stickyfill */
 // Add browser support to position: sticky
